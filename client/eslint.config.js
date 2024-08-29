7ce7668d077a98e88c3399cd51f473211a6724e1
Deyan Bozhilov <dodobrat98@gmail.com>
2024-08-29 22:36:08 +0300

    eslint + prettier config

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,14 +1,16 @@
 import js from "@eslint/js";
-import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
+    files: ["**/*.{ts,tsx,js}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -16,9 +18,36 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react", "^@?\\w"], // React and other external packages
+            ["^~"], // Internal imports (e.g., `~/`)
+            ["^\\.\\.(?!/?$)", "^\\./"], // Parent imports, followed by other relative imports.
+            ["^\\u0000"], // Side effect imports
+            ["^.+\\.?(css)$"], // Style imports
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+      "no-unused-vars": "off",
+      "no-duplicate-imports": "error",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
     },
-  }
+  },
 );
