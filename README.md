1dee814d6c90bbb780d0e14139c5cf99de935884
Deyan Bozhilov <dodobrat98@gmail.com>
2024-08-29 21:16:41 +0300

    init client

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# .
+# `devnote`
 
 To install dependencies:
 
---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui";
+
+export default function App() {
+  return (
+    <>
+      Hello
+      <Button>TEST</Button>
+    </>
+  );
+}
