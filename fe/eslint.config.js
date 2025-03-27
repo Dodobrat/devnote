7219ed9c00feeed67792adac0b463470d63a5c92
Deyan Bozhilov <dodobrat98@gmail.com>
2025-03-27 21:57:35 +0200

    eslint setup

---
+++ fe/eslint.config.js
@@ -0,0 +1,58 @@
+import js from "@eslint/js";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+import pluginRouter from "@tanstack/eslint-plugin-router";
+import reactHooks from "eslint-plugin-react-hooks";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx,js}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
+      "@tanstack/query": pluginQuery,
+      "@tanstack/router": pluginRouter,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react", "^@?\\w"], // React and other external packages
+            ["^~"], // Internal imports (e.g., `~/`)
+            ["^\\.\\.(?!/?$)", "^\\./"], // Parent imports, followed by other relative imports.
+            ["^\\u0000"], // Side effect imports
+            ["^.+\\.?(css)$"], // Style imports
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
+      "no-duplicate-imports": "error",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+);
