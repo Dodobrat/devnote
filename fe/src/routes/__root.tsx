6729b27fd7966d092a5fc49b35c0f36d8ac6fa7f
Deyan Bozhilov <dodobrat98@gmail.com>
2025-03-30 16:22:28 +0300

    fix

--- fe/src/routes/__root.tsx
+++ fe/src/routes/__root.tsx
@@ -61,7 +61,6 @@ import {
   DropdownMenuItem,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
 import { Input } from "~/components/ui/input";
